39b43b708156ad3f95051fea3f4044b738eb3f64
Waleed <waleedthedev@gmail.com>
2021-12-29 20:01:24 +0500

    Setup website according to the provided style guides

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,16 @@
 <script>
 </script>
 
-<main>
+<svelte:head>
+  <title>Time Tracking Dashboard</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
 
+<main class="w-screen min-h-screen">
+  <h1>Time Tracking Dashboard</h1>
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,38 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            blue: 'hsl(246, 80%, 60%)',
+            'light-red-work': 'hsl(15, 100%, 70%)',
+            'soft-blue-play': 'hsl(195, 74%, 62%)',
+            'light-red-study': 'hsl(348, 100%, 68%)',
+            'lime-green-exercise': 'hsl(145, 58%, 55%)',
+            'violet-social': 'hsl(264, 64%, 52%)',
+            'soft-orange-self-care': 'hsl(43, 84%, 65%)',
+          },
+          neutral: {
+            'very-dark-blue': 'hsl(226, 43%, 10%)',
+            'dark-blue': 'hsl(235, 46%, 20%)',
+            'desaturated-blue': 'hsl(235, 45%, 61%)',
+            'pale-blue': 'hsl(236, 100%, 87%)',
+          },
+        },
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
